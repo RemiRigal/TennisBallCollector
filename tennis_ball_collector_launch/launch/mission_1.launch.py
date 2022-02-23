318e316165f2d1db5f8b7c66bc4c564197a65072
Sam <samuel.prouten@ensta-bretagne.org>
2022-02-23 11:28:24 +0100

    Cleaned up repo

--- tennis_ball_collector_launch/launch/mission_1.launch.py
+++ tennis_ball_collector_launch/launch/mission_1.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     urdf_file = os.path.join(get_package_share_directory('robochon_description'), urdf_file_name)
 
     sim_time_arg = DeclareLaunchArgument('use_sim_time',
-                                         default_value='false',
+                                         default_value='true',
                                          description='Use simulation (Gazebo) clock if true')
 
     robot_state_publisher_node = Node(package='robot_state_publisher',
@@ -28,13 +28,6 @@ def generate_launch_description():
                                                   {'use_sim_time': use_sim_time}],
                                       arguments=[urdf_file])
 
-    joint_state_publisher_node = Node(package='joint_state_publisher',
-                                      executable='joint_state_publisher',
-                                      name='joint_state_publisher',
-                                      output='screen',
-                                      parameters=[{'use_sim_time': use_sim_time}]
-                                      )
-
     spawn_node = Node(package='gazebo_ros',
                       executable='spawn_entity.py',
                       name='urdf_spawner',
@@ -44,6 +37,5 @@ def generate_launch_description():
     return LaunchDescription([
         sim_time_arg,
         robot_state_publisher_node,
-        joint_state_publisher_node,
         spawn_node,
     ])
